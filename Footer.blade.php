9bb4355d31f74f7c9a696ac5436cdd19826a214b
kwangchuk1002 <kwangchuk100@gmail.com>
2023-03-14 17:42:57 +0600

    Create Footer.blade.php

---
+++ Footer.blade.php
@@ -0,0 +1,2 @@
+<button type="button" class="button-glow location"><i class="fa fa-map-maker"></i></button>
+<a href="https://www.google.com/maps/dir//Azha+Pasa,+NPPF+Housing+Colony,+Thimphu/@27.4695625,89.6341875,15z/data=!4m9!4m8!1m0!1m5!1m1!1s0x39e1957f363961eb:0xa5b199cddf6b2ce7!2m2!1d89.6341875!2d27.4695625!3e0" class="button-glow location"><i class="fa fa-map-marker"></i></a>
